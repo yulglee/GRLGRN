11757c0b7af82eaf79a9ce1820f877f1e6fa5ace
yulglee <141541916+yulglee@users.noreply.github.com>
2024-10-01 18:40:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from compute_metrics import evaluate_model
 from parser import get_train_parameter
 from parser import parser_add_main_args
 import os
-def fix_seed(seed): # Make sure that all random number generators are affected by the same seed throughout the code, so that the experimental results are reproducible.
+def fix_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
@@ -29,7 +29,6 @@ if Ground_truth_network == 'STRING Dataset':
     operation = 'l2'
 else:
     operation = 'all'
-# Dara path
 exp_data_file = Benchmark_root + '/' + Ground_truth_network + '/' + Cells_type_name + '/' + Gene_num + '/BL--ExpressionData.csv'
 Gene_file = Benchmark_root + '/' + Ground_truth_network + '/' + Cells_type_name + '/' + Gene_num + '/Target.csv'
 TF_list_file = Benchmark_root + '/' + Ground_truth_network + '/' + Cells_type_name + '/' + Gene_num + '/TF.csv'
@@ -39,7 +38,6 @@ test_set_file = Benchmark_root + '/' + Ground_truth_network + '/' + Cells_type_n
 Target_file = Benchmark_root + '/' + Ground_truth_network + '/' + Cells_type_name + '/' + Gene_num + '/Target.csv'
 ground_network_path = Benchmark_root + '/' + Ground_truth_network + '/' + Cells_type_name + '/' + Gene_num + '/Label.csv'
 parameter_path =  'Dataset/' + Ground_truth_network + '/' + Cells_type_name + '/' + Gene_num + '/model_parameter.pth'
-# Read Data
 type_gene_dict, _ = get_type_genes_dict(exp_data_file, TF_list_file)
 Gene_exp, training_adj, training_adj_list, training_pairs_m = get_train_exp_adj_pairs(type_gene_dict,
                                                                                       train_set_file,
@@ -60,14 +58,12 @@ for i, edge in enumerate(training_adj_list):
 A = torch.cat([A, torch.eye(num_nodes).type(torch.FloatTensor).unsqueeze(0)], dim=0)
 training_adjs_m = A.to(args.device)
 node_embedding = torch.from_numpy(Gene_exp).type(torch.FloatTensor).to(args.device)
-# Define model
 model = GRLGRN(num_gene=len(Gene_exp), args=args, num_implicit_edge_types=training_adjs_m.size()[0],
                num_implicit_links=args.num_implicit_links,
                input_dim=feature_dim,
                output_dim=args.output_dim,
                device=args.device).to(args.device)
 node_feature = node_embedding.detach()
-# Load parameter
 checkpoint = torch.load(parameter_path)
 model.load_state_dict(checkpoint)
 metrics = evaluate_model(model, gene_exp=node_feature, gtn_adj=training_adjs_m,
