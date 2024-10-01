fbaabccd515759c971c270bfd120494367d5534b
yulglee <141541916+yulglee@users.noreply.github.com>
2024-10-01 18:41:41 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import numpy as np
 def get_batches(pairs, batch_size):
-    n_batches = (len(pairs) + (batch_size - 1)) // batch_size#先判断有多少个batch数
+    n_batches = (len(pairs) + (batch_size - 1)) // batch_size
 
-    for idx in range(n_batches):#遍历每一个batch的元素
+    for idx in range(n_batches):
         edges, label = [], []
-        for i in range(batch_size):#利用batch_size实现上述目的
+        for i in range(batch_size):
             index = idx * batch_size + i
             if index >= len(pairs):
                 break
@@ -12,7 +12,7 @@ def get_batches(pairs, batch_size):
             label.append(pairs[index][2])
         yield np.array(edges), np.array(label)
 
-def get_mask2index(mask, label=3): # 3-unknown
+def get_mask2index(mask, label=3): 
     nonzero_indices = np.nonzero(mask)
     unlabel = np.ones_like(nonzero_indices[0]) * 3
     unlabel_pair = np.column_stack((nonzero_indices[0], nonzero_indices[1], unlabel))
